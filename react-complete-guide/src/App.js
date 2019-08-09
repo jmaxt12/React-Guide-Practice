28a2ec4bb37bed079a5d648f3d4dd12f1d45f122
Jake Thomas <jmaxt12@gmail.com>
2019-08-09 14:05:42 -0600

    using two different ways to use and argument to change with on click

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
     otherState: 'Something else'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('Was clicked!')
     // console.log('Was clicked!')
     // DON"T DO THIS: this.state.persons[0].name = 'Zookster'
     this.setState({
       persons: [
-        { name: 'Zookster', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Riko', age: 46 },
         { name: 'Darv', age: 32 }
       ]
@@ -29,14 +29,14 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person 
+        <button onClick={() => this.switchNameHandler('Zookster!!')}>Switch Name</button>
+        <Person
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age}/>
         <Person 
             name={this.state.persons[1].name} 
             age={this.state.persons[1].age}
-            click={this.switchNameHandler}>My Hobbies: DMing</Person>
+            click={this.switchNameHandler.bind(this, 'Zook!')}>My Hobbies: DMing</Person>
         <Person 
             name={this.state.persons[2].name} 
             age={this.state.persons[2].age}/>
