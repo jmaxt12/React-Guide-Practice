94c4316a62afdb3654970e7aa061416da1842072
Jake Thomas <jmaxt12@gmail.com>
2019-08-21 17:46:01 -0600

    changed conditional statement to be a better way

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -48,16 +48,12 @@ class App extends Component {
       border: '1px solid blue',
       badding: '8px',
       cursor: 'pointer'
-    }
+    };
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button 
-            style={style}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {this.state.showPersons === true ? 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
         <div>
         <Person
             name={this.state.persons[0].name} 
@@ -70,8 +66,18 @@ class App extends Component {
         <Person 
             name={this.state.persons[2].name} 
             age={this.state.persons[2].age}/>
-        </div> : null
-        }
+        </div>
+      )
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button 
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
       </div>
     );
   }
