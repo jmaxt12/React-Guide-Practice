c26f6e3416533957fce0ec12de2c71e1239a0c5a
Jake Thomas <jmaxt12@gmail.com>
2019-11-04 16:10:34 -0700

    add media query using StyleRoot from radium

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 class App extends Component {
   state = {
@@ -89,6 +89,7 @@ class App extends Component {
     }
 
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
@@ -97,6 +98,7 @@ class App extends Component {
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
       </div>
+      </StyleRoot>
     );
   }
 }
