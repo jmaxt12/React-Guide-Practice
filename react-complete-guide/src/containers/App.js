7fd78cb9f5ab18c2fbf005edc3c4875e842fcd64
Jake Thomas <jmaxt12@gmail.com>
2019-12-13 12:34:46 -0700

    added hoc withClass

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
@@ -90,7 +91,7 @@ class App extends Component {
     
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button 
           onClick={() => {
             this.setState({ showCockpit: false })
@@ -107,7 +108,7 @@ class App extends Component {
           />
          ) : null} 
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
