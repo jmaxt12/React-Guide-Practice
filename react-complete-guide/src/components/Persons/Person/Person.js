a7ecbf5bf7a4f1b9cbd86ae193001fa19d065aba
Jake Thomas <jmaxt12@gmail.com>
2019-12-13 13:05:27 -0700

    added propTypes on Personn.js

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
@@ -13,10 +14,21 @@ class Person extends Component {
         I'm {this.props.name} and I am {this.props.age} years old!
       </p>
       <p>{this.props.children}</p>
-      <input type="text" onChange={this.props.changed} value={this.props.name} />
+      <input 
+        type="text" 
+        onChange={this.props.changed} 
+        value={this.props.name} 
+      />
     </Aux>
   );
 };
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
